ce81a22d41125ab254de8a0c3762941741096a1e
stm32p103 <stm32p103@gmail.com>
2019-04-30 18:56:50 +0900

    再作成

--- nucleo/sw/VCP/RTE/_Target_1/RTE_Components.h
+++ nucleo/sw/VCP/RTE/_Target_1/RTE_Components.h
@@ -16,13 +16,8 @@
  */
 #define CMSIS_device_header "stm32f0xx.h"
 
-/*  ARM::CMSIS:RTOS2:Keil RTX5:Library:5.5.0 */
-#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
-        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
 /*  Keil.STM32CubeMX::Device:STM32Cube Framework:STM32CubeMX:1.0.0 */
 #define RTE_DEVICE_FRAMEWORK_CUBE_MX
-/*  Keil.STM32CubeMX::Device:Startup:2.3.3 */
-#define RTE_DEVICE_STARTUP_STM32F0XX    /* Device Startup for STM32F0 */
 /*  Keil::CMSIS Driver:USB Device:1.0.0 */
 #define RTE_Drivers_USBD0               /* Driver USBD0 */
 
