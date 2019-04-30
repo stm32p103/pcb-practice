40401480dedddebced046bd36627eb16a419c164
stm32p103 <stm32p103@gmail.com>
2019-04-30 15:24:36 +0900

    コード生成前 USBのみ設定

---
+++ nucleo/sw/VCP/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,25 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'vcp' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+/*  Keil.STM32CubeMX::Device:STM32Cube Framework:STM32CubeMX:1.0.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/*  Keil::CMSIS Driver:USB Device:1.0.0 */
+#define RTE_Drivers_USBD0               /* Driver USBD0 */
+
+
+#endif /* RTE_COMPONENTS_H */
