0fef6354addda2a339f914e3361ae951637180d1
stm32p103 <stm32p103@gmail.com>
2019-05-01 22:29:24 +0900

    CubeMX設定変更後

--- nucleo/sw/test/RTE/_Target_1/RTE_Components.h
+++ nucleo/sw/test/RTE/_Target_1/RTE_Components.h
@@ -23,8 +23,6 @@
 #define RTE_DEVICE_FRAMEWORK_CUBE_MX
 /*  Keil.STM32CubeMX::Device:STM32Cube HAL:1.7.0 */
 #define RTE_DEVICE_CUBE_MX_HAL
-/*  Keil.STM32CubeMX::Device:Startup:2.3.3 */
-#define RTE_DEVICE_STARTUP_STM32F0XX    /* Device Startup for STM32F0 */
 /*  Keil::CMSIS Driver:CAN:1.0.0 */
 #define RTE_Drivers_CAN1                /* Driver CAN1 */
 /*  Keil::CMSIS Driver:USB Device:1.0.0 */
