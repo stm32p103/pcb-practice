47a3f46a0b19938db4ecc05f2d4a0e3b3b358b1a
stm32p103 <stm32p103@gmail.com>
2019-05-02 21:08:19 +0900

    CAN設定修正(500kbps)

--- nucleo/sw/CAN/RTE/Device/STM32F072RBTx/STCubeGenerated/Src/main.c
+++ nucleo/sw/CAN/RTE/Device/STM32F072RBTx/STCubeGenerated/Src/main.c
@@ -163,11 +163,11 @@ static void MX_CAN_Init(void)
 
   /* USER CODE END CAN_Init 1 */
   hcan.Instance = CAN;
-  hcan.Init.Prescaler = 16;
-  hcan.Init.Mode = CAN_MODE_NORMAL;
-  hcan.Init.SJW = CAN_SJW_1TQ;
-  hcan.Init.BS1 = CAN_BS1_1TQ;
-  hcan.Init.BS2 = CAN_BS2_1TQ;
+  hcan.Init.Prescaler = 6;
+  hcan.Init.Mode = CAN_MODE_LOOPBACK;
+  hcan.Init.SJW = CAN_SJW_4TQ;
+  hcan.Init.BS1 = CAN_BS1_7TQ;
+  hcan.Init.BS2 = CAN_BS2_8TQ;
   hcan.Init.TTCM = DISABLE;
   hcan.Init.ABOM = DISABLE;
   hcan.Init.AWUM = DISABLE;
