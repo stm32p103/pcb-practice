50d4c7b6eba1f6462f32138e5a55d37f9063646d
stm32p103 <stm32p103@gmail.com>
2019-05-02 20:25:23 +0900

    一旦削除

--- nucleo/sw/VCP/RTE/Device/STM32F072RBTx/STCubeGenerated/MDK-ARM/RTE/_STCubeGenerated/RTE_Components.h
+++ nucleo/sw/VCP/RTE/Device/STM32F072RBTx/STCubeGenerated/MDK-ARM/RTE/_STCubeGenerated/RTE_Components.h
@@ -16,6 +16,12 @@
  */
 #define CMSIS_device_header "stm32f0xx.h"
 
+/*  Keil.STM32CubeMX::Device:STM32Cube Framework:STM32CubeMX:1.0.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/*  Keil.STM32CubeMX::Device:STM32Cube HAL:1.7.0 */
+#define RTE_DEVICE_CUBE_MX_HAL
+/*  Keil::CMSIS Driver:CAN:1.0.0 */
+#define RTE_Drivers_CAN1                /* Driver CAN1 */
 
 
 #endif /* RTE_COMPONENTS_H */
