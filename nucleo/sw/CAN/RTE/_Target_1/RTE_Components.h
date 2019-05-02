7b9c81cd74a15cd09a8cecf72b7ef59e2242c9c8
stm32p103 <stm32p103@gmail.com>
2019-05-02 20:29:58 +0900

    CANプロジェクト作成

---
+++ nucleo/sw/CAN/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,30 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'CAN' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+/*  ARM::CMSIS:RTOS2:Keil RTX5:Library:5.5.0 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+/*  Keil.STM32CubeMX::Device:STM32Cube Framework:STM32CubeMX:1.0.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/*  Keil.STM32CubeMX::Device:Startup:2.3.3 */
+#define RTE_DEVICE_STARTUP_STM32F0XX    /* Device Startup for STM32F0 */
+/*  Keil::CMSIS Driver:CAN:1.0.0 */
+#define RTE_Drivers_CAN1                /* Driver CAN1 */
+
+
+#endif /* RTE_COMPONENTS_H */
