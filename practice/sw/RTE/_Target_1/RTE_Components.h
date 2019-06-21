393c0e1a3bdbd0332b7f1b04852eac61dd67f889
stm32p103 <stm32p103@gmail.com>
2019-06-21 21:31:34 +0900

    ソフトのベース追加

---
+++ practice/sw/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,33 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'sample' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+/*  ARM::CMSIS:RTOS2:Keil RTX5:Library:5.5.0 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+/*  Keil.STM32CubeMX::Device:STM32Cube Framework:STM32CubeMX:1.0.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/*  Keil.STM32CubeMX::Device:STM32Cube HAL:1.7.0 */
+#define RTE_DEVICE_CUBE_MX_HAL
+/*  Keil::CMSIS Driver:CAN:1.0.0 */
+#define RTE_Drivers_CAN1                /* Driver CAN1 */
+/*  Keil::CMSIS Driver:I2C:1.0.0 */
+#define RTE_Drivers_I2C1                /* Driver I2C1 */
+        #define RTE_Drivers_I2C2                /* Driver I2C2 */
+
+
+#endif /* RTE_COMPONENTS_H */
